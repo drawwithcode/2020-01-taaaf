7d649681959251b3be5b70aca1b68e8ff7ca3300
taaaf <mattia.tafel@gmail.com>
2020-09-21 22:13:19 +0200

    wrote code v1

--- sketch.js
+++ sketch.js
@@ -3,10 +3,70 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
+  createCanvas(windowWidth,windowHeight);
   // put setup code here
+ angleMode(DEGREES);
+
+ rectMode(CENTER);
+
+
 }
 
 function draw() {
   // put drawing code here
-}
+
+
+
+
+    noStroke();
+
+    if(frameCount%2==0){
+
+      fill("yellow")
+
+    } else {
+
+      fill("blue");
+
+    }
+
+/*
+
+    if(keyIsPressed==true&&frameCount%2==0){
+
+      background("red");
+
+    }
+
+
+    if(keyIsPressed==true&&frameCount%2==1){
+
+      background("magenta");
+
+    }
+
+    */
+
+    if(keyIsPressed==false){
+
+      background(lerpColor(color("black"), color("white"), mouseX/1500));
+
+    }
+
+
+    push();
+
+    translate(width / 2, height / 2);
+
+    rotate(frameCount * 2);
+
+    scale(sin(frameCount));
+
+    rect(0, 0, 500, 500);
+
+    pop();
+
+
+
+
+  }
